95b915ff3447af70b4e36e000b4a68203a71d217
Ankux <ankuxkr@gmail.com>
2024-04-01 11:12:38 +0530

    u

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res)=>{
-    res.send("Hello");
+    res.render('index');
+});
+
+app.get('/users/login', (req, res)=>{
+    res.render('login');
+});
+
+app.get('/users/register', (req, res)=>{
+    res.render('register');
+});
+
+app.get('/users/dashboard', (req, res)=>{
+    res.render('dashboard', {user: "Ankush"});
 });
 
 app.listen(PORT, ()=>console.log(`Server Started at ${PORT}`));
