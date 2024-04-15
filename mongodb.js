5d9878f897de2c00246d07235068383e51b5528c
Ankux <ankuxkr@gmail.com>
2024-04-15 11:15:51 +0530

    added write, post and view blog feature

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { types } = require('pg');
+
 
 mongoose.connect("mongodb://localhost:27017/wellbeingdb")
 .then(()=>{
@@ -9,6 +9,7 @@ mongoose.connect("mongodb://localhost:27017/wellbeingdb")
     console.log('failded to connect');
 })
 
+//Log in Schema
 const LogInSchema = new mongoose.Schema({
     name:{
         type: String,
@@ -23,8 +24,32 @@ const LogInSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-})
+});
 
 const collection = new mongoose.model("users", LogInSchema);
 
-module.exports = collection;
+
+//Blog Post Schema
+const BlogPostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const BlogPost = mongoose.model("BlogPost", BlogPostSchema);
+
+
+module.exports = { collection, BlogPost }
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const path = require('path');
-const collection = require('./mongodb');
+const { collection, BlogPost } = require('./mongodb');
 const PORT = process.env.PORT || 8000;
 
-app.set('view engine', 'ejs'); //middleware
+
+//middlewae
+app.set('view engine', 'ejs'); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname,"public")));
@@ -34,6 +36,9 @@ function ensureAuthenticated(req, res, next) {
     }
 }
 
+
+
+//All routes
 app.get('/', ensureNotAuthenticated, (req, res)=>{
     res.render('index');
 });
@@ -49,7 +54,6 @@ app.get('/users/register', (req, res)=>{
 });
 
 
-
 app.get('/users/dashboard', ensureAuthenticated, (req, res)=>{
     res.render('dashboard', { userName: req.session.user.name });
 });
@@ -59,20 +63,45 @@ app.get('/quiz', (req, res)=>{
     res.render('quiz');
 });
 
-app.get('/users/blog', ensureAuthenticated, (req, res)=>{
-    res.render('blog');
+
+app.get('/users/blog', ensureAuthenticated, async (req, res)=>{
+    try {
+        const blogPosts = await BlogPost.find({}).sort({ createdAt: -1 });
+        res.render('blog', { blogPosts });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
 });
+
 
 app.get('/users/writeblog', ensureAuthenticated, (req, res)=>{
     res.render('writeblog', { userName: req.session.user.name });
 });
 
 
-app.get('/users/blog/view-post', ensureAuthenticated, (req, res)=>{
-    res.render('view-post', {  });
+app.get('/users/blog/view-post/:id', ensureAuthenticated, async (req, res)=>{
+    try {
+        const blogPost = await BlogPost.findById(req.params.id);
+        if (!blogPost) {
+            return res.status(404).send('Post not found');
+        }
+        res.render('view-post', { blogPost });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
 });
 
 
+app.get('/users/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect('/users/dashboard');
+        }
+        res.redirect('/');
+    });
+});
 
 
 app.post('/users/register', async (req, res)=>{
@@ -87,13 +116,15 @@ app.post('/users/register', async (req, res)=>{
     res.redirect('/users/dashboard');
 })
 
+
 app.post('/users/login', async (req, res)=>{
     try {
         const check = await collection.findOne({email:req.body.email});
-        if (check) {
-            if( check.password == req.body.password) { //ensuring check is not null
+        if (check) { //ensuring check is not null: user is registered
+            if( check.password == req.body.password) { //ensuring the password is correct
                 req.session.user = { name: check.name };
                 res.redirect('/users/dashboard');
+
             }
             else{
                 res.render('login', { errorMessage: "Incorrect Password" });
@@ -101,23 +132,32 @@ app.post('/users/login', async (req, res)=>{
         } else {
             res.render('login', { errorMessage: "User not found" });
         }
+
     } catch (error) {
+        console.log(error);
         res.render('login', { errorMessage: "An error occured" });
     }
-
 })
 
 
-app.get('/users/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.redirect('/users/dashboard');
-        }
-        res.redirect('/');
-    });
+app.post('/users/writeblog', async (req, res) => {
+    const blogPost = {
+        title: req.body.title,
+        content: req.body.content,
+        author: req.session.user.name 
+    }
+
+    try {
+        await BlogPost.insertMany([blogPost]);
+        res.redirect('/users/blog');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
 });
 
-// Catch all handler: the last route
+
+// Catch all routes that are not defined: the last route
 app.use((req, res, next) => {
     res.status(404).render('404', { title: '404 - Page Not Found' });
 });
